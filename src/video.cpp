7773e55089b5af03b549141c37906db287f78835
Firobe <virgile.robles@protonmail.ch>
2016-03-18 21:26:47 +0100

    Lignes non nécessaires

--- src/video.cpp
+++ src/video.cpp
@@ -40,9 +40,9 @@ Video::Video(int width, int height, string a, string b) {
 		throw runtime_error("Unable to initialize GLFW");
 
 	glfwWindowHint(GLFW_SAMPLES, 4); //Anti-aliasing
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	_window = glfwCreateWindow(width, height, "BeatRacer", NULL, NULL);
 
 	if (!_window)
